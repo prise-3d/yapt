bd4a8cb8855d175e7644049fe605a7844028ab8d
Franck Vandewiele <franckvandewiele@gmail.com>
2024-06-09 15:35:27 +0200

    defocus blur

--- include/camera.h
+++ include/camera.h
@@ -7,6 +7,7 @@
 
 #include "yapt.h"
 #include "hittable.h"
+#include <png.h>
 
 class camera {
 public:
@@ -20,6 +21,9 @@ public:
     point3 lookat   = point3(0,0,-1);  // Point camera is looking at
     vec3   vup      = vec3(0,1,0);     // Camera-relative "up" direction
 
+    double defocus_angle = 0;  // Variation angle of rays through each pixel
+    double focus_dist = 10;    // Distance from camera lookfrom point to plane of perfect focus
+
     void render(const hittable& world);
 
 private:
@@ -29,6 +33,8 @@ private:
     vec3   pixel_delta_u;        // Offset to pixel to the right
     vec3   pixel_delta_v;        // Offset to pixel below
     vec3   u, v, w;              // Camera frame basis vectors
+    vec3   defocus_disk_u;       // Defocus disk horizontal radius
+    vec3   defocus_disk_v;       // Defocus disk vertical radius
     double pixel_samples_scale;  // Color scale factor for a sum of pixel samples
 
     void initialize();
@@ -38,5 +44,56 @@ private:
     ray get_ray(int i, int j) const;
 
     vec3 sample_square() const;
+
+    point3 defocus_disk_sample() const;
 };
+
+inline bool write_png(const std::string& file_name, const std::vector<uint8_t>& image_data, int width, int height) {
+    FILE* fp = fopen(file_name.c_str(), "wb");
+    if (!fp) {
+        std::cerr << "Erreur d'ouverture du fichier" << std::endl;
+        return false;
+    }
+
+    png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (!png_ptr) {
+        std::cerr << "Erreur de création de la structure png" << std::endl;
+        fclose(fp);
+        return false;
+    }
+
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+    if (!info_ptr) {
+        std::cerr << "Erreur de création de la structure d'information png" << std::endl;
+        png_destroy_write_struct(&png_ptr, NULL);
+        fclose(fp);
+        return false;
+    }
+
+    if (setjmp(png_jmpbuf(png_ptr))) {
+        std::cerr << "Erreur lors de l'écriture de l'image PNG" << std::endl;
+        png_destroy_write_struct(&png_ptr, &info_ptr);
+        fclose(fp);
+        return false;
+    }
+
+    png_init_io(png_ptr, fp);
+    png_set_IHDR(png_ptr, info_ptr, width, height,
+                 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
+                 PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+
+    std::vector<png_bytep> row_pointers(height);
+    for (int y = 0; y < height; y++) {
+        row_pointers[y] = const_cast<png_bytep>(&image_data[y * width * 3]);
+    }
+
+    png_set_rows(png_ptr, info_ptr, row_pointers.data());
+    png_write_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
+
+    png_destroy_write_struct(&png_ptr, &info_ptr);
+    fclose(fp);
+
+    return true;
+}
+
 #endif //YAPT_CAMERA_H
