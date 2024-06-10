9d538869b71c41d67e386602543ada82797d0f22
Franck Vandewiele <franckvandewiele@gmail.com>
2024-06-10 10:32:25 +0200

    quad class

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,44 @@
 #include <chrono>
 #include "texture.h"
 #include "rtw_stb_image.h"
+#include "quad.h"
+
+void quads() {
+    hittable_list world;
+
+    // Materials
+    auto left_red     = make_shared<lambertian>(color(1.0, 0.2, 0.2));
+    auto back_green   = make_shared<lambertian>(color(0.2, 1.0, 0.2));
+    auto right_blue   = make_shared<lambertian>(color(0.2, 0.2, 1.0));
+    auto upper_orange = make_shared<lambertian>(color(1.0, 0.5, 0.0));
+    auto lower_teal   = make_shared<lambertian>(color(0.2, 0.8, 0.8));
+
+    // Quads
+    world.add(make_shared<quad>(point3(-3,-2, 5), vec3(0, 0,-4), vec3(0, 4, 0), left_red));
+    world.add(make_shared<quad>(point3(-2,-2, 0), vec3(4, 0, 0), vec3(0, 4, 0), back_green));
+    world.add(make_shared<quad>(point3( 3,-2, 1), vec3(0, 0, 4), vec3(0, 4, 0), right_blue));
+    world.add(make_shared<quad>(point3(-2, 3, 1), vec3(4, 0, 0), vec3(0, 0, 4), upper_orange));
+    world.add(make_shared<quad>(point3(-2,-3, 5), vec3(4, 0, 0), vec3(0, 0,-4), lower_teal));
+
+    camera cam;
+
+    cam.aspect_ratio      = 1.0;
+    cam.image_width       = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth         = 50;
+
+    cam.vfov     = 80;
+    cam.lookfrom = point3(0,0,9);
+    cam.lookat   = point3(0,0,0);
+    cam.vup      = vec3(0,1,0);
+    auto a = .7;
+    cam.background = (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(world);
+}
+
 
 void perlin_spheres() {
     hittable_list world;
@@ -201,12 +239,13 @@ auto im = image_texture("earthmap.jpg");
 }
 
 int main() {
-    switch(6) {
+    switch(7) {
         case 1: spheres(); break;
         case 2: earth(); break;
         case 3: img(); break;
         case 4: two_spheres(); break;
         case 5: two_metal_spheres(); break;
         case 6: perlin_spheres(); break;
+        case 7: quads(); break;
     }
 }
