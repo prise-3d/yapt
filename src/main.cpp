4c8d2277b0c60fec135f1fab3f4c580532891f9c
Franck Vandewiele <franckvandewiele@gmail.com>
2024-09-27 10:46:14 +0200

    dummy commit

--- src/main.cpp
+++ src/main.cpp
@@ -185,4 +185,5 @@ int main(int argc, char* argv[]) {
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
     std::clog << "Rendering duration: " << double(duration.count()) / 1000. << " s" << std::endl;
+    std::clog << "Commit: ne pas le prendre" << std::endl;
 }
