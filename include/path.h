274898413c45614a4574e0357b4f96a147104712
Franck Vandewiele <franckvandewiele@gmail.com>
2024-11-13 11:40:06 +0100

    feat: pre pathconnector

--- include/path.h
+++ include/path.h
@@ -23,4 +23,56 @@ public:
         ++depth;
     }
 };
+
+class PathGuidingStrategy {
+    public:
+    virtual ~PathGuidingStrategy() = default;
+
+    PathGuidingStrategy(shared_ptr<Hittable> scene, shared_ptr<Hittable> lights, size_t max_depth): scene(scene), lights(lights), max_depth(max_depth) {};
+
+    virtual void grow(shared_ptr<Path> start, shared_ptr<Path> end) ;
+
+    shared_ptr<Hittable> scene;
+    shared_ptr<Hittable> lights;
+    size_t max_depth;
+};
+
+class SimpleGuidingStrategy: public PathGuidingStrategy {
+    public:
+
+    ~SimpleGuidingStrategy() = default;
+
+    SimpleGuidingStrategy(shared_ptr<Hittable> scene, shared_ptr<Hittable> lights, size_t max_depth): PathGuidingStrategy(scene, lights, max_depth) {};
+
+    void grow(shared_ptr<Path> start, shared_ptr<Path> end) override {
+        //TODO: do something dumb here
+    }
+};
+
+
+class PathConnector {
+public:
+    virtual ~PathConnector() = default;
+
+    PathConnector(size_t max_depth): max_depth(max_depth) {};
+
+    // tries to connect start with end
+    // in case of failure, relies on strategy to
+    virtual shared_ptr<Path> connect(Path& start, Path& end, PathGuidingStrategy* strategy) {
+        while (start.depth + end.depth < start.records.size()) {
+            /*if (visible (start, end)) {
+                return shared_ptr<>(start.concatenate(end.reversed()))
+            } else {
+
+                strategy.grow(start, end);
+            }*/
+        }
+        //FIXME: we have a problem
+        return nullptr;
+    }
+
+    size_t max_depth;
+
+};
+
 #endif //YAPT_PATH_H
