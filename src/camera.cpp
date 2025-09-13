a0730cb755b0a7b964cab7ab3f7f0206c8339152
Franck Vandewiele <franckvandewiele@gmail.com>
2025-09-13 17:05:30 +0200

    fix: next event estimation now correctly relies on a power heuristic MIS

--- src/camera.cpp
+++ src/camera.cpp
@@ -154,6 +154,7 @@ Color ForwardCamera::rayColor(const Ray& r, const int depth, const Hittable& wor
 
     // Next Event Estimation (NEE)
     if (this->use_nee) {
+
         // Sample a point on the light
         auto light_ptr = make_shared<HittablePDF>(lights, rec.p);
         Ray light_ray(rec.p, light_ptr->generate());
@@ -163,28 +164,63 @@ Color ForwardCamera::rayColor(const Ray& r, const int depth, const Hittable& wor
             HitRecord light_rec;
             // Check if the light is visible (no occlusion)
             world.hit(light_ray, Interval(0.001, INFINITY), light_rec);
-            if (light_rec.t > 0.999) {
-                if (light_rec.mat->emitted(light_ray, light_rec, light_rec.u, light_rec.v, light_rec.p).length2() > 0) {
+            if (light_rec.t > 0.9999) {
+                Color light_emission = light_rec.mat->emitted(light_ray, light_rec, light_rec.u, light_rec.v, light_rec.p);
+                if (light_emission.length2() > 0) {
                     double scattering_pdf = rec.mat->scattering_pdf(r, rec, light_ray);
-                    Color light_emission = light_rec.mat->emitted(light_ray, light_rec, light_rec.u, light_rec.v, light_rec.p);
-                    colorFromScatter += scatterRecord.attenuation * scattering_pdf * light_emission / light_pdf;
+
+                    // BALANCE HEURISTIC
+                    // double weight_nee = light_pdf / (light_pdf + scattering_pdf);
+                    // colorFromScatter += weight_nee * scatterRecord.attenuation * scattering_pdf * light_emission / light_pdf;
+
+                    // POWER HEURISTIC (beta = 2)
+                    double light_pdf_2 = light_pdf * light_pdf;
+                    double scattering_pdf_2 = scattering_pdf * scattering_pdf;
+                    double weight_nee = light_pdf_2 / (light_pdf_2 +scattering_pdf_2);
+                    colorFromScatter += weight_nee * scatterRecord.attenuation * scattering_pdf * light_emission / light_pdf;
                 }
             }
         }
+
+        // Standard path tracing
+        const auto scattered = Ray(rec.p, scatterRecord.pdf_ptr->generate());
+        const auto brdf_pdf = scatterRecord.pdf_ptr->value(scattered.direction());
+
+        if (brdf_pdf > 0) {
+            const double scatteringPdf = rec.mat->scattering_pdf(r, rec, scattered);
+            const Color sampleColor = rayColor(scattered, depth - 1, world, lights);
+
+
+            auto light_ptr_for_weight = make_shared<HittablePDF>(lights, rec.p);
+            double light_pdf_for_this_direction = light_ptr_for_weight->value(scattered.direction());
+
+            // BALANCE HEURISTIC
+            // double weight_pt = brdf_pdf / (light_pdf_for_this_direction + brdf_pdf);
+
+            // POWER HEURISTIC (beta = 2)
+            double light_pdf_2 = light_pdf_for_this_direction * light_pdf_for_this_direction;
+            double brdf_pdf_2 = brdf_pdf * brdf_pdf;
+            double weight_pt = brdf_pdf_2 / (light_pdf_2 + brdf_pdf_2);
+
+            colorFromScatter += weight_pt * (scatterRecord.attenuation * scatteringPdf * sampleColor) / brdf_pdf;
+        }
+    } else {
+        // Standard path tracing
+        const auto light_ptr = make_shared<HittablePDF>(lights, rec.p);
+        // const MixturePDF p(light_ptr, scatterRecord.pdf_ptr);
+
+        const MixturePDF p(light_ptr, scatterRecord.pdf_ptr);
+
+        const auto scattered = Ray(rec.p, p.generate());
+        const auto pdfValue = p.value(scattered.direction());
+
+        const double scatteringPdf = rec.mat->scattering_pdf(r, rec, scattered);
+
+        const Color sampleColor = rayColor(scattered, depth - 1, world, lights);
+        colorFromScatter += (scatterRecord.attenuation * scatteringPdf * sampleColor) / pdfValue;
+
+        return color_from_emission + colorFromScatter;
     }
-    
-    // Standard path tracing
-    const auto light_ptr = make_shared<HittablePDF>(lights, rec.p);
-    const MixturePDF p(light_ptr, scatterRecord.pdf_ptr);
-
-    const auto scattered = Ray(rec.p, p.generate());
-    const auto pdfValue = p.value(scattered.direction());
-
-    const double scatteringPdf = rec.mat->scattering_pdf(r, rec, scattered);
-
-    const Color sampleColor = rayColor(scattered, depth - 1, world, lights);
-    colorFromScatter += (scatterRecord.attenuation * scatteringPdf * sampleColor) / pdfValue;
-
     return color_from_emission + colorFromScatter;
 }
 
