1ec302a0d811ca95aed9a7a3f22220f00a6cf293
Franck Vandewiele <franckvandewiele@gmail.com>
2025-10-12 15:27:13 +0200

    feat: hovering a qtvor voronoi cell now highlights the corresponding sample in the sample table

--- include/qtvor/voronoicellitem.h
+++ include/qtvor/voronoicellitem.h
@@ -14,15 +14,19 @@
 #include <QTableWidget>
 #include <QHeaderView>
 #include <QClipboard>
+#include <QMouseEvent>
 
 class VoronoiCellItem : public QGraphicsPolygonItem {
 public:
     VoronoiCellItem(const QPolygonF &polygon, const QBrush &brush, const QPointF &site,
-                    const qreal ellipseDiameter, QGraphicsItem *parent = nullptr)
+                    const qreal ellipseDiameter, const int rowIndex, QTableWidget *table,
+                    QGraphicsItem *parent = nullptr)
         : QGraphicsPolygonItem(polygon, parent),
           originalBrush(brush),
           sitePoint(site),
-          ellipseDiameter(ellipseDiameter) {
+          ellipseDiameter(ellipseDiameter),
+          rowIndex(rowIndex),
+          tableWidget(table) {
         setBrush(brush);
         setAcceptHoverEvents(true);
 
@@ -41,6 +45,12 @@ protected:
         setBrush(QBrush(Qt::yellow));
         QGraphicsPolygonItem::hoverEnterEvent(event);
 
+        // Highlight the corresponding row in the table
+        if (tableWidget && rowIndex >= 0 && rowIndex < tableWidget->rowCount()) {
+            tableWidget->selectRow(rowIndex);
+            tableWidget->scrollToItem(tableWidget->item(rowIndex, 0));
+        }
+
         auto views = scene()->views();
         if (!views.isEmpty()) {
             if (QGraphicsView *view = views.first()) {
@@ -53,6 +63,11 @@ protected:
     void hoverLeaveEvent(QGraphicsSceneHoverEvent *event) override {
         setBrush(originalBrush);
         QGraphicsPolygonItem::hoverLeaveEvent(event);
+
+        // Deselect the table row
+        if (tableWidget) {
+            tableWidget->clearSelection();
+        }
     }
 
 private:
@@ -60,6 +75,8 @@ private:
     QPointF sitePoint;
     qreal ellipseDiameter;
     QGraphicsEllipseItem *ellipseItem;
+    int rowIndex;
+    QTableWidget *tableWidget;
 };
 
 class CustomTableWidget : public QTableWidget {
@@ -112,8 +129,6 @@ private:
 };
 
 inline void displayVoronoi(Scene yaptScene, int x, int y) {
-    auto *qScene = new QGraphicsScene();
-
     auto ag = yaptScene.camera->renderPixel(*yaptScene.content, *yaptScene.lights, y, x);
 
     auto aggregator = std::dynamic_pointer_cast<VoronoiAggregator>(ag);
@@ -124,6 +139,34 @@ inline void displayVoronoi(Scene yaptScene, int x, int y) {
 
     auto delaunay = aggregator->delaunay;
     auto voronoi = aggregator->voronoi;
+
+    // Create the table first so we can pass it to VoronoiCellItem
+    size_t size = aggregator->samples.size();
+    auto *table = new CustomTableWidget(size, 6);
+    table->setHorizontalHeaderLabels({"Site x", "Site y", "R",
+                                  "G", "B", "Area"});
+
+    // Populate the table
+    size_t row = 0;
+    for (auto vertex = delaunay.vertices_begin(); vertex != delaunay.vertices_end(); ++vertex) {
+        Point p = vertex->point();
+
+        Color contribution = aggregator->contributions[row];
+        table->setItem(row, 0, new QTableWidgetItem(QString("%1").arg(p.x())));
+        table->setItem(row, 1, new QTableWidgetItem(QString("%1").arg(p.y())));
+        table->setItem(row, 2, new QTableWidgetItem(QString("%1").arg(contribution.x())));
+        table->setItem(row, 3, new QTableWidgetItem(QString("%1").arg(contribution.y())));
+        table->setItem(row, 4, new QTableWidgetItem(QString("%1").arg(contribution.z())));
+        table->setItem(row, 5, new QTableWidgetItem(QString("%1").arg(aggregator->weights[row])));
+        ++row;
+    }
+
+    table->horizontalHeader()->setStretchLastSection(true);
+    table->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    table->setEditTriggers(QAbstractItemView::NoEditTriggers);
+
+    // Now create the graphics scene with cells linked to table rows
+    auto *qScene = new QGraphicsScene();
 
     QPen voronoiPen(Qt::blue);
     voronoiPen.setWidthF(.0002);
@@ -165,7 +208,7 @@ inline void displayVoronoi(Scene yaptScene, int x, int y) {
         auto *cellItem = new VoronoiCellItem(
             qPolygon, QBrush(toQColor(aggregator->contributions[idx])),
             QPointF(site.x(), site.y()),
-            ellipse_diameter);
+            ellipse_diameter, idx, table);
         cellItem->setPen(voronoiPen);
         qScene->addItem(cellItem);
         ++idx;
@@ -185,31 +228,6 @@ inline void displayVoronoi(Scene yaptScene, int x, int y) {
     QVBoxLayout *textLayout = new QVBoxLayout(textWidget);
 
     textLayout->addWidget(new QLabel(QString("Pixel: (%1, %2)").arg(x).arg(y)));
-
-    size_t size = aggregator->samples.size();
-
-    auto *table = new CustomTableWidget(size, 6);
-    table->setHorizontalHeaderLabels({"Site x", "Site y", "R",
-                                  "G", "B", "Area"});
-
-    size_t row = 0;
-    for (auto vertex = delaunay.vertices_begin(); vertex != delaunay.vertices_end(); ++vertex) {
-        Point p = vertex->point();
-
-        Color contribution = aggregator->contributions[row];
-        table->setItem(row, 0, new QTableWidgetItem(QString("%1").arg(p.x())));
-        table->setItem(row, 1, new QTableWidgetItem(QString("%1").arg(p.y())));
-        table->setItem(row, 2, new QTableWidgetItem(QString("%1").arg(contribution.x())));
-        table->setItem(row, 3, new QTableWidgetItem(QString("%1").arg(contribution.y())));
-        table->setItem(row, 4, new QTableWidgetItem(QString("%1").arg(contribution.z())));
-        table->setItem(row, 5, new QTableWidgetItem(QString("%1").arg(aggregator->weights[row])));
-        ++row;
-    }
-
-    table->horizontalHeader()->setStretchLastSection(true);
-    table->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
-    table->setEditTriggers(QAbstractItemView::NoEditTriggers);
-
     textLayout->addWidget(table);
 
 
